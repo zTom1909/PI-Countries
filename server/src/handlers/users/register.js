1844f6480c399e6faf38bd9fe4181416f39becab
zTom1909 <topolococo2@gmail.com>
2023-09-11 03:18:04 -0300

    Handled duplicated registers

--- server/src/handlers/users/register.js
+++ server/src/handlers/users/register.js
@@ -1,6 +1,8 @@
 const { User } = require("../../db");
 
 module.exports = async (user) => {
+  const oldUser = await User.findOne({ where: { email: user.email } });
+  if (oldUser) throw new Error("That user already exists!")
   const newUser = await User.create(user);
   return newUser.dataValues;
 };
